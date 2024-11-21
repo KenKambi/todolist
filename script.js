8d379ea10786f3d7fff4597b3f7f019e0165559d
Ken Kambi <kenkambi43@gmail.com>
2024-11-21 18:45:25 +0300

    arrays and loops

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const myArray=[''];
-
-
+const myArray=[{
+    name:'book flight',
+    dueDate:'12-11-2024'
+    }, {
+    name: 'buy cars',
+    dueDate: '12-11-2024'
+    }];
 
 function clickList(){
     let taskNameHTML='';
 
     for (let i= 0; i <myArray.length; i++){
-            const taskName = myArray[i];
-            const html = `<p> ${taskName} </p>`; //create some HTML code
+            const taskNameObject = myArray[i];
+            //const name = taskNameObject.name;
+            //const dueDate = taskNameObject.dueDate; Destructuring
+            const{name, dueDate} = taskNameObject;
+
+
+            const html = `<p> ${name} ${dueDate} 
+                        <button onclick="
+                        myArray.splice(${i},1);
+                        clickList();"
+                        >Delete</button>
+            </p>`; //generating some HTML code
             taskNameHTML +=html;
     }
 
@@ -18,8 +32,17 @@ document.querySelector('.js-list').innerHTML = `${taskNameHTML}`;
 //Input box
 function inputEvent(){
     const inputElelemt = document.querySelector('.js-input');
+    const dateElelemt = document.querySelector('.js-date');
+
     const name = inputElelemt.value;
-    myArray.push(name);
+    const dueDate = dateElelemt.value;
+    myArray.push({
+        //name: name,
+        //dueDate: dueDate [shorthand syntax]
+        name,
+        dueDate
+    });
+
     console.log(myArray);
     inputElelemt.value='';
     clickList();
